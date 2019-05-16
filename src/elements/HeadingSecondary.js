c75d7fece05e5728349e0a9f80cab7030451d7a8
ylor <2200609+ylor@users.noreply.github.com>
2019-05-16 15:54:41 -0400

    Update HeadingSecondary.js

--- src/elements/HeadingSecondary.js
+++ src/elements/HeadingSecondary.js
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 export default styled.h2`
   display: block;
   font-size: 32px;
+  letter-spacing: -1.5px;
   line-height: 1.25;
   margin-bottom: 2.5vh;
-  letter-spacing: -1.5px;
   text-align: center;
-  text-transform: uppercase;
 
   @media (max-width: 768px) {
     font-size: 30px;
